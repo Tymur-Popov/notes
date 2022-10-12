c6db173ffa7bcc4c9c6e21ce377a3a7c24f13dfe
Tymur Popov <bs2timur@gmail.com>
2022-10-12 13:51:12 +0300

    update

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState } from "react";
+
+//components
+import Header from "./components/Header/Header";
+import CreateArea from "./components/CreateArea/CreateArea";
+import Note from "./components/Note/Note";
+import Count from "./components/Count/Count";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevValue) => {
+      return [...prevValue, newNote];
+    });
+  }
+
+  function deleteNotes (id) {
+    setNotes(prevValue => {
+      return [...prevValue.filter((note, index) => 
+        index !== id )]
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Count count={notes.length === 0? "Empty" : ` ${notes.length} Notes in Database`}/>
+      <CreateArea onAdd={addNote} />
+      {notes.map((note, index) => (
+        <Note
+          key={index}
+          id={index}
+          tittle={note.tittle}
+          content={note.content}
+          onDelete={deleteNotes}
+        />
+      ))}
     </div>
   );
 }
